3f5a331a7302dd9b7e02d4857b436044b26d4ff1
brunomneto <mullernetobruno@gmail.com>
2022-09-12 23:46:02 -0300

    teste

---
+++ desafio_codigo/teste.java
@@ -0,0 +1,14 @@
+public class teste {
+    public static void main(String[] args) {
+        // TODO: Crie um Array de números inteiros:
+    int [] numeros = {2, 3, 5, 7, 11, 13, 18, 34};
+    int i; 
+     // TODO: Implemente um laço de repetição que verifique APENAS seus números Pares:
+          for (i = 0; i < numeros.length; i++){
+              if (numeros[i] % 2 == 0){
+                      System.out.println(numeros[i]);
+                     } 
+                 }
+                
+         }
+    }
